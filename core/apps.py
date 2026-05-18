c89aa9a806e72c6fd6d6fd7c9bdb45e86cddf497
Jave Sumatra <javesumatra2@gmail.com>
2026-05-19 00:10:33 +0800

    fix admin login render properly

--- core/apps.py
+++ core/apps.py
@@ -6,4 +6,18 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        import core.signals  # ✅ connects your signals (e.g., auto-create Student)
+        import core.signals
+
+        # force admin creation safely on startup
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(email="admin@gmail.com").exists():
+            User.objects.create_superuser(
+                email="admin@gmail.com",
+                password="123",
+                is_staff=True,
+                is_superuser=True,
+                is_active=True
+            )
