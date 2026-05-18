a1a7f415237c62a65ac7970fedc562ef762159e8
Jave Sumatra <javesumatra2@gmail.com>
2026-05-18 23:24:59 +0800

    deploy ready backend

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn enrollment_system.wsgi:application
--- enrollment_system/settings.py
+++ enrollment_system/settings.py
@@ -32,6 +32,9 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # MIDDLEWARE
 MIDDLEWARE = [
@@ -106,6 +109,7 @@ USE_TZ = True
 # STATIC FILES
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # MEDIA FILES
